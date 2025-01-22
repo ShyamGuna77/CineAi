5a55d63bc68dc6b978385f24b986eed3211d65fc
Shyam <gshyamprasad2001@gmail.com>
2025-01-22 17:50:39 +0530

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
-
-import './App.css'
+import React from 'react';
+import {CssBaseline} from '@mui/material';
 
 function App() {
   
 
   return (
     <>
-   
+    <div>
+ <CssBaseline />
+ <h1>CineAi</h1>
+
+    </div>
     </>
   )
 }

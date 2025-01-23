f2f2984db0b73062f843945c857231fba3005fca
Shyam <gshyamprasad2001@gmail.com>
2025-01-23 11:12:18 +0530

    aadded comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
 
-import React from 'react';
+
+
 import {CssBaseline} from '@mui/material';
+import{Routes, Route} from 'react-router-dom';
+import Movies from './components/Movies';
+import MovieInformation from './components/MovieInformation';
+import Actors from './components/Actors';
+import Profile from './components/Profile';
+import Navbar from './components/Navbar';
+
 
 function App() {
   
 
   return (
     <>
-    <div>
- <CssBaseline />
- <h1>CineAi</h1>
-
-    </div>
+      <div className='flex  h-full'>
+        <CssBaseline />
+        <Navbar />
+        <main className='grow p-8'>
+        <div className='h-[70px]'>
+          <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/movie/:id" element={<MovieInformation />} />
+            <Route path="/actors/:id" element={<Actors />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Routes>
+          </div>
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
